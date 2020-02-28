c58f70f2090eddc267e110182abc6557f967a36d
lSwarml <karelskij.maks@mail.ru>
2020-02-29 00:48:11 +0300

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <form className="form">
+                <h1>Обратная связь</h1>
+                <div className='FIO'>
+                    <input required className='textbox' placeholder='Фамилия'/>
+                    <input required style={{marginRight:2+"%",marginLeft:2+"%"}} className='textbox' placeholder='Имя'/>
+                    <input  className='textbox' placeholder='Отчество'/>
+                </div>
+                <div>
+                    <input required className='textbox' type="tel" placeholder='Телефон'/>
+                </div>
+                <div>
+                    <input required className='textbox' type="email" placeholder='Email'/>
+                </div>
+                <div>
+                    <textarea required className='message' placeholder='Сообщение:'/>
+                </div>
+                <button className='button'>Отправить</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
